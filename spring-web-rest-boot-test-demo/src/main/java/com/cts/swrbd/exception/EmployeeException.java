a41634b5823fa725b32bdf0865232622f8282784
pcbuilderayush <pcbuilderayush@gmail.com>
2020-02-27 16:50:56 +0530

    Add files via upload

---
+++ spring-web-rest-boot-test-demo/src/main/java/com/cts/swrbd/exception/EmployeeException.java
@@ -0,0 +1,8 @@
+package com.cts.swrbd.exception;
+
+@SuppressWarnings("serial")
+public class EmployeeException extends Exception {
+	public EmployeeException(String errorMessage) {
+		super(errorMessage);
+	}
+}
---
+++ spring-web-rest-boot-test-demo/src/main/java/com/cts/swrbd/model/Employee.java
@@ -0,0 +1,140 @@
+package com.cts.swrbd.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long empId;
+	
+	@NotEmpty(message="First name can not be empty")
+	@NotNull(message="First name can not be omiteed")
+	private String firstName;
+	
+	@NotEmpty(message="last name can not be empty")
+	@NotNull(message="last name can not be omiteed")
+	private String lastName;
+	
+	@Min(value = 25000,message="basic can not be less than 25 thousand")
+	@Max(value = 150000, message="basic can not be more than 4.5 lakh")
+	private double basic;
+	
+	@DateTimeFormat(iso=ISO.DATE)
+	private LocalDate joinDate;
+	
+	@Enumerated(EnumType.STRING)
+	private Department dept;
+	
+	@Pattern(regexp="[1-9][0-9]{9}",message="Mobile number should be of 10 digits")
+	@NotNull(message="Mobile Number Cant be omitted")
+	private String mobileNumber;
+	
+	@Email(message="email must be valid")
+	@NotNull(message="Email cant be omiteed")
+	private String email;
+
+	public Long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getBasic() {
+		return basic;
+	}
+
+	public void setBasic(double basic) {
+		this.basic = basic;
+	}
+
+	public LocalDate getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(LocalDate joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public Department getDept() {
+		return dept;
+	}
+
+	public void setDept(Department dept) {
+		this.dept = dept;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Employee(@NotEmpty(message = "First name can not be empty") @NotNull(message = "First name can not be omiteed") String firstName,
+			@NotEmpty(message = "last name can not be empty") @NotNull(message = "last name can not be omiteed") String lastName,
+			@Min(value = 25000, message = "basic can not be less than 25 thousand") @Max(value = 150000, message = "basic can not be more than 4.5 lakh") double basic,
+			LocalDate joinDate, Department dept,
+			@Pattern(regexp = "[1-9][0-9]{9}", message = "Mobile number should be of 10 digits") @NotNull(message = "Mobile Number Cant be omitted") String mobileNumber,
+			@Email(message = "email must be valid") @NotNull(message = "Email cant be omiteed") String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.basic = basic;
+		this.joinDate = joinDate;
+		this.dept = dept;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+	}
+	
+	
+	
+	
+
+}
